cd765373a442192c442dbb1eaa8ecd8ee61226a6
Shabab <s7abab2k1@gmail.com>
2024-01-04 20:08:40 +0530

    updated valuation srv

--- valuation/src/server.ts
+++ valuation/src/server.ts
@@ -3,7 +3,7 @@ import connectDb from "./frameworks/config/db";
 require("dotenv").config();
 
 // create server
-app.listen(process.env.PORT || 8003, () => {
+app.listen(8003, () => {
   connectDb();
   console.log(`Server running ${process.env.PORT}`);
 });
