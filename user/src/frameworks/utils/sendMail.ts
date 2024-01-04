a53efa95e105a54c3fe812a3ea92e2ae9e433b6b
Shabab <s7abab2k1@gmail.com>
2024-01-04 21:11:08 +0530

    updated user srv

--- user/src/frameworks/utils/sendMail.ts
+++ user/src/frameworks/utils/sendMail.ts
@@ -13,11 +13,11 @@ interface EmailOptions {
 const sendMail = async (options: EmailOptions): Promise<void> => {
   const transporter: Transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT ||"587"),
+    port: parseInt(process.env.SMTP_PORT || "587"),
     service: process.env.SMTP_SERVICE,
     auth: {
       user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD,
+      pass: "nfqa qlrp fyxs pzky",
     },
   });
   const { email, subject, template, data } = options;
